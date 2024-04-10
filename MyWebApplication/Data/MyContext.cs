1b48a7156c50b22550ee9a8e5d38cef95bfe0f43
ykiyoshima <y.kiyoshima.271@gmail.com>
2024-04-10 19:21:19 +0900

    [add] ユーザー画面の追加

---
+++ MyWebApplication/Data/MyContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyWebApplication.Models;
+
+namespace MyWebApplication.Data
+{
+    public class MyContext : DbContext
+    {
+        public MyContext (DbContextOptions<MyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyWebApplication.Models.User> User { get; set; } = default!;
+    }
+}
---
+++ MyWebApplication/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyWebApplication.Data;
+using MyWebApplication.Models;
+
+namespace MyWebApplication.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyWebApplication.Data.MyContext _context;
+
+        public IndexModel(MyWebApplication.Data.MyContext context)
+        {
+            _context = context;
+        }
+
+        public IList<User> User { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            User = await _context.User.ToListAsync();
+        }
+    }
+}
